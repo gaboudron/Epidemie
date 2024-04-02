2a224356bcd353316e4efb4b7f5829667db33bc9
Gabriel BOUDRON <gabriel.boudron@etu.uca.fr>
2024-04-02 22:19:56 +0200

    déplacer dans src

---
+++ src/Test.java
@@ -0,0 +1,11 @@
+class Test
+{
+    public static void main(String[] args)
+    {
+        Twister instanceRandom = new Twister(42);
+        Monde m = new Monde(instanceRandom);
+        for(int i = 0; i < 100; i++){
+            m.runSimulation(i);
+        }  
+    }
+}
