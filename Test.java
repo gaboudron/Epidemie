50497ae2977c9b078f3f6b55ec6791a66e0b373d
Gabriel BOUDRON <gabriel.boudron@etu.uca.fr>
2024-04-02 20:55:19 +0200

    correction d'un bug, int i

--- Test.java
+++ Test.java
@@ -1,12 +1,13 @@
-
+package src;
 
 class Test
 {
-   public static void main(String[] args){
-   	    Twister instanceRandom = new Twister(42);
+    public static void main(String[] args)
+    {
+        Twister instanceRandom = new Twister(42);
         Monde m = new Monde(instanceRandom);
-        for(i = 0; i< 100; i++){
+        for(int i = 0; i < 100; i++){
             m.runSimulation(i);
         }  
-   }
+    }
 }
