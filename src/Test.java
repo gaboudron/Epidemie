ef719933595015a409c1718bfdc4e509afcad308
Gabriel BOUDRON <gabriel.boudron@etu.uca.fr>
2024-04-04 17:25:10 +0200

    changement de la seed

--- src/Test.java
+++ src/Test.java
@@ -1,10 +1,15 @@
 package src;
+
 class Test
 {
     public static void main(String[] args)
     {
+        int[] init = {0x123, 0x234, 0x345, 0x456};
+        
+        Twister instanceRandom = new Twister();
+        instanceRandom.init_by_array(init);
+        
         for(int i = 0; i < 100; i++){
-            Twister instanceRandom = new Twister(42);
             Monde m = new Monde(instanceRandom);
             m.runSimulation(i);
         }  
