16ad9a143138d4feb87ed33acc73b9566f125da6
Gabriel BOUDRON <gabriel.boudron@etu.uca.fr>
2024-04-04 13:44:45 +0200

    Modification boucle for

--- src/Test.java
+++ src/Test.java
@@ -3,9 +3,9 @@ class Test
 {
     public static void main(String[] args)
     {
-        Twister instanceRandom = new Twister(42);
-        Monde m = new Monde(instanceRandom);
         for(int i = 0; i < 100; i++){
+            Twister instanceRandom = new Twister(42);
+            Monde m = new Monde(instanceRandom);
             m.runSimulation(i);
         }  
     }
