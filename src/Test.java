430a3092d7166f250768518ac665c0d0fc27d236
Gabriel BOUDRON <gabriel.boudron@etu.uca.fr>
2024-04-07 22:58:25 +0200

    mode compatible

--- src/Test.java
+++ src/Test.java
@@ -4,11 +4,9 @@ class Test
 {
     public static void main(String[] args)
     {
-        int[] init = {0x123, 0x234, 0x345, 0x456};
-        
-        Twister instanceRandom = new Twister();
-        instanceRandom.init_by_array(init);
-        
+        // l'argument 'true' permet d'agir comme dans le code original en C
+        Twister instanceRandom = new Twister(true);
+
         for(int i = 0; i < 100; i++){
             Monde m = new Monde(instanceRandom);
             m.runSimulation(i);
